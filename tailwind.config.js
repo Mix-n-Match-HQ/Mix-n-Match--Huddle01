c02b86cf59896251ec0afac4d4b1c131328fb833
Nikhil Verma <nv5475609@gmail.com>
2023-05-04 18:58:28 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'], 
+  safelist: [
+    'bg-black',
+    'bg-white',
+    'text-white',
+    'text-black',
+    {
+      pattern: /bg-(red|green|blue|gray|orange|yellow|sky|blue|purple|pink)-(100|200|300|400|500)/,
+    },
+  ],
   theme: {
     extend: {},
   },
